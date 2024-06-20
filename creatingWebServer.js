2258c54e30fe62e3df53c10f145beea47e308bd7
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-06-20 13:17:11 +0530

    this is adding the new home, about, content,  in router

--- creatingWebServer.js
+++ creatingWebServer.js
@@ -4,10 +4,21 @@ const http = require('http');
 
 let show =fs.readFileSync('./basis_templates/learnServer.html','utf-8')
 const server = http.createServer((req, res) => {
-    res.end(show);
+    // res.end(show);
     // console.log(show);
     // console.log(req);
     // console.log(res);
+    let path = req.url;
+    if(path==='/' || path.toLocaleLowerCase()==='/home'){
+        res.end('you are in home page');
+}else if( path.toLocaleLowerCase()==='/about'){
+    res.end('you are in about page');
+
+}
+else if( path.toLocaleLowerCase()==='/content'){
+    res.end('you are in content page');
+
+}
 });
 
 server.listen(8000, '127.0.0.1', () => {
