812788b6b16f3213137cd4e3ed702627fd53b107
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-06-20 11:11:45 +0530

    (learn the adding the html file in the server)

--- creatingWebServer.js
+++ creatingWebServer.js
@@ -2,11 +2,12 @@ const readline = require('readline');
 const fs = require('fs');
 const http = require('http');
 
+let show =fs.readFileSync('./basis_templates/learnServer.html','utf-8')
 const server = http.createServer((req, res) => {
-    // res.end('hello server');
-    console.log('New request');
-    console.log(req);
-    console.log(res);
+    res.end(show);
+    // console.log(show);
+    // console.log(req);
+    // console.log(res);
 });
 
 server.listen(8000, '127.0.0.1', () => {
